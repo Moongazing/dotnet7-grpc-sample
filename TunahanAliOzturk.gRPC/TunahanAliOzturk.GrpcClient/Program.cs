301d2f70ee4e90cecbdf8b95891e04db33c59c2a
Moongazing <72943708+Moongazing@users.noreply.github.com>
2023-06-10 00:52:44 +0300

    Add project files.

---
+++ TunahanAliOzturk.gRPC/TunahanAliOzturk.GrpcClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using TunahanAliOzturk.GrpcServer;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        //var input = new HelloRequest { Name = "Tuna" };
+
+        //var channel = GrpcChannel.ForAddress("https://localhost:7277");
+
+        //var client = new Greeter.GreeterClient(channel);
+
+        //var reply = await client.SayHelloAsync(input);
+
+        //Console.WriteLine(reply.Message);
+
+        Console.ReadLine();
+    }
+}
